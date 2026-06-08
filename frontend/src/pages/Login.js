6c0b6ec2b531492bf2c3ac637f7b344f1726fa4b
Comrade-Holmes <santhoshravi1903@gmail.com>
2026-06-08 23:47:05 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,18 +1,22 @@
-
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
 
 export default function Login() {
-  const [form, setForm] = useState({ email: "", password: "" });
+  const [form, setForm] = useState({
+    email: "",
+    password: ""
+  });
+
   const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-  `${process.env.REACT_APP_API_URL}/api/auth/login`,
-  form
-);
+        `${process.env.REACT_APP_API_URL}/api/auth/login`,
+        form
+      );
+
       localStorage.setItem("token", res.data.token);
       navigate("/dashboard");
     } catch (err) {
@@ -24,10 +28,27 @@ export default function Login() {
     <div className="container">
       <div className="card">
         <h1>Login</h1>
-        <input placeholder="Email" onChange={(e) => setForm({...form, email: e.target.value})}/>
-        <input type="password" placeholder="Password" onChange={(e) => setForm({...form, password: e.target.value})}/>
+
+        <input
+          placeholder="Email"
+          onChange={(e) =>
+            setForm({ ...form, email: e.target.value })
+          }
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) =>
+            setForm({ ...form, password: e.target.value })
+          }
+        />
+
         <button onClick={handleLogin}>Login</button>
-        <p>New user? <Link to="/register">Register</Link></p>
+
+        <p>
+          New user? <Link to="/register">Register</Link>
+        </p>
       </div>
     </div>
   );
